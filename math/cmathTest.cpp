755ef3ba57574c311cd4d438726d200d88fad320
baron-Endeavor <baronysyong@gmail.com>
2017-01-25 16:37:51 +0900

    Add math/cmathTest.cpp

---
+++ math/cmathTest.cpp
@@ -0,0 +1,17 @@
+/*!
+ * @file  cmathTest.cpp
+ * @brief test cmath
+ * @date 2017/01/24
+ * @author YoonSeong Yong 
+ * 
+ */
+
+#include <cmath>
+#include <stdio.h>
+#define _USE_MATH_DEFINES
+
+int main(){
+    printf("PI = %f\n", M_PI);
+    printf("cos(PI) = %f\n", cos(M_PI));
+    printf("acos(-1) = %f\n", acos(-1));
+}
